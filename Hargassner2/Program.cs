51d41849323a85ab5b4a5ad03a43c0d380bdc5a9
Juergen Goschke <jg@ggse.de>
2022-03-10 20:38:17 +0100

    Sicherung

---
+++ Hargassner2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hargassner2
+{
+    class Program
+    {
+        static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            logger.Info("Guten Tach");
+            logger.Trace("Guten Trace");
+            Console.WriteLine("nach'm logging");
+            System.IO.File.Create("/app/log/test.xxx");
+        }
+    }
+}
